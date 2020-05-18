30c5bcfa7492aeb1432d30ad800991242a9b1d41
VChouliak <chouliak@hotmail.com>
2020-05-18 13:18:54 +0200

    Update UserManager

--- NoteList/src/demo/model/UserManager.java
+++ NoteList/src/demo/model/UserManager.java
@@ -6,42 +6,39 @@ import java.util.Optional;
 
 import demo.repositories.implementations.UserRepository;
 
-public class UserManager
-{
-   private static UserManager instance = new UserManager();
-   
-   public static UserManager getInstance()
-   {
-      return UserManager.instance;
-   }
+public class UserManager {
+	private static UserManager instance = new UserManager();
+	private Map<String, User> users = new HashMap<>();
+	private int userIdCounter = 1;
 
-   private Map<String, User> users = new HashMap<>();
-   private int userIdCounter = 1;
+	private UserManager() {
+		super();
+		fillUsersMap();
+	}
 
-   private UserManager()
-   {
-      super();
-   }
+	public static UserManager getInstance() {
+		return UserManager.instance;
+	}
+	
+	private void fillUsersMap() {
+		if (!UserRepository.getInstance().getAll().isEmpty())
+			UserRepository.getInstance().getAll().forEach(user -> users.put(user.getUsername(), user));
+	}
 
-   public Optional<User> lookupUser(String username)
-   {
-      return this.users.values().stream().filter(user -> user.getUsername().equals(username)).findFirst();
-   }
+	public Optional<User> lookupUser(String username) {
+		return this.users.values().stream().filter(user -> user.getUsername().equals(username)).findFirst();
+	}
 
-   public User register(String username, String password, String email)
-   {
-      if (this.lookupUser(username).isPresent())
-      {
-         RuntimeException exce = new RuntimeException("User " + username + " exists");
-         throw exce;
-      }
-      else
-      {
-         User user = new User(userIdCounter,username, password, email);
-         this.users.put(username, user);
-         UserRepository.getInstance().create(user);
-         userIdCounter++;
-         return user;
-      }
-   }
+	public User register(String username, String password, String email) {
+		if (this.lookupUser(username).isPresent()) {
+			RuntimeException exce = new RuntimeException("User " + username + " exists");
+			throw exce;
+		} else {
+			User user = new User(userIdCounter, username, password, email);
+			this.users.put(username, user);
+			UserRepository.getInstance().create(user);
+			userIdCounter++;
+			return user;
+		}
+	}
 }
